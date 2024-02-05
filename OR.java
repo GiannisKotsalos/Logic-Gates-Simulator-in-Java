d47fcc2146f465f188ae322444ec5da23e4d19a6
GiannisKotsalos <122018348+GiannisKotsalos@users.noreply.github.com>
2024-02-05 15:00:17 +0200

    Update OR.java

---
+++ OR.java
@@ -0,0 +1,25 @@
+package LogicGates;
+
+public class OR {
+    private int input1x;
+    private int input1y;
+    private int input2x;
+    private int input2y;
+    
+    
+
+    public OR(int input1x, int input1y, int input2x,int input2y) {
+        this.input1x = input1x;
+        this.input1y = input1x;
+        this.input2x = input2x;
+        this.input2y = input2y;
+       
+    }
+
+    public void calcOutput(boolean[][] bbholes, int outputRow,int outputColumn ) {
+        boolean result = bbholes[input1x][input1y] || bbholes[input2x][input2y];
+        bbholes[outputRow][outputColumn] = result;
+    }
+}
+
+		
